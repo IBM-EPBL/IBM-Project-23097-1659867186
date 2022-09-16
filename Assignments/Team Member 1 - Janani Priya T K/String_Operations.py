713452610a00c41131f3163f1e55fcc1dca5b877
Jananipriya074 <jsvtrk1970@gmail.com>
2022-09-16 23:54:24 +0530

    done

---
+++ Assignments/Team Member 1 - Janani Priya T K/String_Operations.py
@@ -0,0 +1,7 @@
+from operator import concat
+
+input1=input("Enter String1 : ")
+input2=input("Enter String2 : ")
+print("After concatenation : ",concat(input1,input2))
+print("After reversal of String1 : ",input1[::-1])
+print("Slicing form 1-3 : ",input1[1:3])
